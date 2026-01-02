d6950030d78a7a5e625b8b401ba9bdc6232022be
Priyanshu <priyanshu12656@gmail.com>
2026-01-02 23:51:32 +0530

    wrong data

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ const page = () => {
       <div className='w-full flex justify-center items-center bg-green-400'>
         <div className='max-w-1/2'>
         hello
+        why this not working
         </div>
       </div>
     </div>
