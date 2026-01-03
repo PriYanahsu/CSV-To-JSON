be96a247cba66ec949cd73630dbe85f05e91f4ea
Priyanshu <priyanshu12656@gmail.com>
2026-01-03 23:31:18 +0530

    add validation

--- src/app/hooks/useConvertValue.ts
+++ src/app/hooks/useConvertValue.ts
@@ -1,22 +1,24 @@
 
 export function useConvertValue(csv: string): string {
 
-    const rows = csv.split('\n').filter(row=> row.trim() !== '');
-    if(rows.length === 0) return '';
+    const rows = csv.split('\n').filter(row => row.trim() !== '');
+    if (rows.length === 0) return '';
 
     const headers = rows[0].split(',').filter(head => head.trim() !== '');
 
     const ans = [];
 
-    for(let i=1; i<rows.length; i++) {
+    for (let i = 1; i < rows.length; i++) {
+
+        if (rows[i].length == 0) return '';
 
         const value = rows[i].split(',').filter(val => val.trim() != '');
-        const obj: {[key: string]: string} = {};
+        const obj: { [key: string]: string } = {};
 
         headers.forEach((header, index) => {
             obj[header] = value[index] ?? '';
         })
         ans.push(obj);
     }
-  return JSON.stringify(ans, null, 2);
+    return JSON.stringify(ans, null, 2);
 }
